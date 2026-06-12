923c6e091ad48ee035d5d5435dd5335836c9a5b9
joshuaromkes12345 <54424@hoornbeeck.nl>
2026-06-12 23:47:19 +0200

    heb media toegevoegd zonder css

--- media.php
+++ media.php
@@ -1,12 +1,35 @@
 <?php 
 require_once 'inc/header.php';
 
-
+$key = 1;
 $soort = htmlspecialchars($_GET['soort']);
+$bestand = "./media/" . $soort;
+$open = scandir($bestand);
 
 echo '<h2 class="soortTitel">' . $soort . '</h2>';
 
 
+
 ?>
+
+<table>
+    <tr>
+        <th>Index</th>
+        <th>Naam</th>
+    </tr>
+    <?php 
+    foreach ($open as $media) {
+        if ($media !== '.' && $media !== '..'){
+            echo '
+            <tr>
+                <td>'. $key . ' </td>
+                <td><a href="#">'. $media . '</a></td>
+            </tr>
+            ';
+            $key++;
+        }
+    }
+    ?>
+</table>
 
 <?php require_once 'inc/footer.php';?>
